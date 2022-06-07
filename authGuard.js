b4f6ce9ca67e0da2e53935f582de0a3d10c43cc2
NeAndrew <andrey1mosckalenko@gmail.com>
2022-06-07 16:13:09 +0300

    fix error2

--- authGuard.js
+++ authGuard.js
@@ -1,7 +1,9 @@
 
 const jwt = require('jsonwebtoken');
 
-export function checkToken(token){
+function checkToken(token){
     const payload = jwt.verify(token, process.env.JWT_SECRET)
     return payload
 }
+
+module.exports = { checkToken }
